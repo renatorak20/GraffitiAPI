bd8b9cb957920694e74e79241e10a992b0d0577d
Renato Rak <renatorak20@gmail.com>
2024-09-21 21:03:59 +0200

    Initial commit

---
+++ src/main/kotlin/com/graffiti/graffitiapi/GraffitiApiApplication.kt
@@ -0,0 +1,11 @@
+package com.graffiti.graffitiapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraffitiApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraffitiApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/graffiti/graffitiapi/GraffitiApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.graffiti.graffitiapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraffitiApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
